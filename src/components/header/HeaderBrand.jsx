df9bcc1655f68a96fc2636695db4d711a7fa33d3
Syntrice <syntrice@protonmail.com>
2025-02-19 21:08:57 +0000

    feat: Rename site to Chef Bot

--- src/components/header/HeaderBrand.jsx
+++ src/components/header/HeaderBrand.jsx
@@ -3,6 +3,6 @@ import brandImageURL from '@/assets/chef-claude-icon.png'
 export default () => (
     <div className='flex items-center gap-5'>
         <img width={50} src={brandImageURL} alt="A stencil drawing of a robot chef." />
-        <h1 className='text-4xl'>Chef Claude</h1>
+        <h1 className='text-4xl'>Chef Bot</h1>
     </div>
 )
