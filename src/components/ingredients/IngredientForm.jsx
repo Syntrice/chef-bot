b3a7a2dcd6049a641a8de87fee3d8db6f5c3ee69
Syntrice <syntrice@protonmail.com>
2025-02-19 11:30:41 +0000

    refactor: Use action for IngredientForm submission

--- src/components/ingredients/IngredientForm.jsx
+++ src/components/ingredients/IngredientForm.jsx
@@ -1,14 +1,8 @@
 export default ({ onSubmit }) => {
-    
-    function submit(e) {
-        e.preventDefault()
-        const formData = new FormData(e.currentTarget)
-        const ingredient = formData.get("ingredient")
-        onSubmit(ingredient)
-    }
-    
+
     return (
-        <form onSubmit={submit} className="flex gap-5 py-15 flex-wrap">
+        <form action={(formData) => onSubmit(formData.get("ingredient"))}
+            className="flex gap-5 py-15 flex-wrap">
             <input
                 className="textbox"
                 type="text"
