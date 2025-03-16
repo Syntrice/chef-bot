03fe3fef7ee708626e8a8a73988062c2bb83d0ca
Sam Maughan <syntrice@protonmail.com>
2025-03-16 13:38:36 +0000

    fix: fix typo in Recipe.jsx

--- src/components/recipie/Recipie.jsx
+++ src/components/recipie/Recipie.jsx
@@ -3,7 +3,7 @@ import Markdown from "react-markdown"
 export default ( { recipieContents }) => {
     return (
         <>
-        <h2 className="section-title">Suggested recipie:</h2>
+        <h2 className="section-title">Suggested recipe:</h2>
         <article className="typography">
             <Markdown>
                 {recipieContents}
